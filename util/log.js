b58a404362f9cf77bafef9dd48787238e901ca50
serguun42 <s42@serguun42.ru>
2022-12-22 19:45:41 +0300

    Version 2.7.1 – Updated logging

--- util/log.js
+++ util/log.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-console */
 import { writeFile } from 'fs/promises';
+import { inspect } from 'util';
 import IS_DEV from './is-dev.js';
 
+const SHORT_DELIMITER = Array.from({ length: 30 }, () => '~').join('');
+const LONG_DELIMITER = Array.from({ length: 30 }, () => '🔼').join('');
+
 /**
- * @param  {(string | Error)[]} args
+ * @param {...any} args
+ * @returns {string}
+ */
+const WrapForOutput = (...args) =>
+  args.map((arg) => inspect(arg, { depth: Infinity, colors: true })).join(`\n${SHORT_DELIMITER}\n`);
+
+/**
+ * @param  {...(string | Error)} args
  * @returns {void}
  */
 const LogMessageOrError = (...args) => {
@@ -11,10 +22,11 @@ const LogMessageOrError = (...args) => {
     (message) => message instanceof Error || (typeof message === 'string' && /test/i.test(message))
   );
   const out = containsError ? console.error : console.log;
+  const wrapped = WrapForOutput(...args);
 
   out(new Date());
-  out(...args);
-  out(Array.from({ length: 30 }, () => '~').join(''));
+  out(wrapped);
+  out(LONG_DELIMITER);
 
   if (IS_DEV) writeFile('./out/logmessageorerror.json', JSON.stringify(args, false, '\t')).catch(console.warn);
 };
